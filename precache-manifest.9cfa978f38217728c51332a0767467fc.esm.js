3c43502b77d1e4f3b0ba875a69531da2b7a2a728
Keonwoo Kim <utophii@kaist.ac.kr>
2019-07-23 08:47:43 +0900

    Updates

---
+++ precache-manifest.9cfa978f38217728c51332a0767467fc.esm.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/preact-transition--preact/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/preact-transition--preact/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/preact-transition--preact/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/preact-transition--preact/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/preact-transition--preact/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/preact-transition--preact/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "cad4d593786702047abf",
+    "url": "/preact-transition--preact/bundle.64692.css"
+  },
+  {
+    "revision": "f32d17e60d046883c2291d6dbc1fb248",
+    "url": "/preact-transition--preact/bundle.f15c0.esm.js"
+  },
+  {
+    "revision": "e46efee1a2a5b181dc502e223dad2894",
+    "url": "/preact-transition--preact/index.html"
+  },
+  {
+    "revision": "79b3e080b1775f6ad3e604a243d80b6e",
+    "url": "/preact-transition--preact/polyfills.e3d3c.esm.js"
+  },
+  {
+    "revision": "269b1b89b15d5da99688",
+    "url": "/preact-transition--preact/route-home/index.tsx.chunk.12605.css"
+  },
+  {
+    "revision": "9cada7a0abfd60ddb22cde837a5e691d",
+    "url": "/preact-transition--preact/route-home/index.tsx.chunk.9524e.esm.js"
+  },
+  {
+    "revision": "ac71d8f755cf7077d2bf",
+    "url": "/preact-transition--preact/route-profile/index.tsx.chunk.1492a.css"
+  },
+  {
+    "revision": "b4b3073e7eb1e8414ec19a497778190b",
+    "url": "/preact-transition--preact/route-profile/index.tsx.chunk.a29c1.esm.js"
+  }
+]);
---
+++ precache-manifest.a82ac322252b366e7202ae7524bbc5e1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/preact-transition--preact/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/preact-transition--preact/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/preact-transition--preact/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/preact-transition--preact/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/preact-transition--preact/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/preact-transition--preact/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "cad4d593786702047abf",
+    "url": "/preact-transition--preact/bundle.64692.css"
+  },
+  {
+    "revision": "cad4d593786702047abf",
+    "url": "/preact-transition--preact/bundle.cad4d.js"
+  },
+  {
+    "revision": "e46efee1a2a5b181dc502e223dad2894",
+    "url": "/preact-transition--preact/index.html"
+  },
+  {
+    "revision": "57f29f8d6687ea70092e",
+    "url": "/preact-transition--preact/polyfills.57f29.js"
+  },
+  {
+    "revision": "269b1b89b15d5da99688",
+    "url": "/preact-transition--preact/route-home/index.tsx.chunk.12605.css"
+  },
+  {
+    "revision": "269b1b89b15d5da99688",
+    "url": "/preact-transition--preact/route-home/index.tsx.chunk.269b1.js"
+  },
+  {
+    "revision": "ac71d8f755cf7077d2bf",
+    "url": "/preact-transition--preact/route-profile/index.tsx.chunk.1492a.css"
+  },
+  {
+    "revision": "ac71d8f755cf7077d2bf",
+    "url": "/preact-transition--preact/route-profile/index.tsx.chunk.ac71d.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/preact-transition--preact/sw-debug.js"
+  },
+  {
+    "revision": "085f19e6fcd68ed11e08c672c59a76be",
+    "url": "/preact-transition--preact/sw-esm.js"
+  },
+  {
+    "revision": "c65054cd727f6332b4231de0fd1e4020",
+    "url": "/preact-transition--preact/sw.js"
+  }
+]);
